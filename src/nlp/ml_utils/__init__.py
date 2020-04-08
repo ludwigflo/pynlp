51028d20d369d3fc275ced75127938a24bb8f17d
Florian Ludwig <florian.ludwig@konicaminolta.eu>
2020-04-09 00:55:05 +0200

    Changed the import within the __init__.py module

--- src/nlp/ml_utils/__init__.py
+++ src/nlp/ml_utils/__init__.py
@@ -1,2 +1,3 @@
+from .src.ml_utils.experiment_structure.structure_utils import init_experiment_dir
 from .src.ml_utils.data_utils import data_split, statistics
-from .src.ml_utils.experiment_structure import utils
+from .src.ml_utils.logging import tb_log
