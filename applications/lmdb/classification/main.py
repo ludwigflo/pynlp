e8d4fb725c223b8e54b073a0d767b32b13b995a3
Florian Ludwig <florian.ludwig@konicaminolta.eu>
2020-04-09 21:43:33 +0200

    First training of the simple classifier

--- applications/lmdb/classification/main.py
+++ applications/lmdb/classification/main.py
@@ -2,6 +2,9 @@ from nlp.text_embeddings.embedding import EmbeddingAlgorithm
 from nlp.utils import read_parameter_file
 import nlp.ml_utils as ml_utils
 from typing import Union, List
+from torch.optim import Adam
+import numpy as np
+import models
 import torch
 import csv
 
@@ -84,11 +87,73 @@ def initialize_experiment(params: dict) -> Union[tuple, str]:
         return exp_root_dir
 
 
+def main(params: dict) -> None:
+    """
+    Main function for training.
+    TODO: Add embedding update to optimizer
+    Parameters
+    ----------
+    params: Training parameters.
+    """
+
+    # get some parameters
+    num_epochs = params['training']['num_epochs']
+    batch_size = params['data_loader']['batch_size']
+    num_iterations = params['training']['num_iterations']
+    learning_rate = float(params['training']['optimizer']['lr'])
+    update_embeddings = params['training']['update_embeddings']
+    betas = tuple([float(x) for x in params['training']['optimizer']['betas']])
+
+    # create a new experiment directory
+    # root_dir, logger_list = initialize_experiment(params)
+    print('\n\nInitialized experiment directory')
+
+    # create a data loader object and extract the train and validation loaders
+    data_loader = DataLoader(25000, params, shuffle_data = True)
+    data_loader.embeddings.set_embedding_gradient(update_embeddings)
+    train_loader = data_loader.train_generator(batch_size = batch_size)
+
+    # initialize a new pytorch model
+    feature_size = data_loader.embeddings.embedding.weight.data.size()[1]
+    model = models.LinearClassifier(feature_size)
+
+    # create an optimizer
+    optim = Adam(model.parameters(), lr = learning_rate, betas = betas)
+
+    # utilize the gpu, if possible
+    cuda = torch.cuda.is_available()
+    if cuda:
+        model = model.cuda()
+
+    # train the model
+    for epoch in range(num_epochs):
+        for i, (data, label) in enumerate(train_loader):
+
+            # utilize the gpu, if possible
+            if cuda:
+                data, label = data.float().cuda(), label.float().cuda()
+
+            # compute the model's predictions
+            prediction = model(data)
+
+            # compute the loss
+            loss = ml_utils.bce_loss_pytorch(prediction, label, logits = False)
+            print(epoch, i, np.asscalar(loss.detach().cpu().numpy()))
+
+            # backpropagate the loss and update the weights
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            # abort criterion
+            if i == num_iterations:
+                break
+
 if __name__ == '__main__':
 
     # read the parameters
     parameter_file = 'params.yaml'
     p = read_parameter_file(parameter_file)
-    # initialize_experiment(p)
-    loader = DataLoader(25000, p, True)
-    emb, lab = loader.load_data([3, 5, 7, 8])
+
+    # run the training
+    main(p)
