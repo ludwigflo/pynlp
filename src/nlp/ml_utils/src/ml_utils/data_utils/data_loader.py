cae11cc330248d203675ef8d8ebfc372705006d0
Florian Ludwig <florian.ludwig@konicaminolta.eu>
2020-04-09 15:59:26 +0200

    Updated Requirements.txt file

---
+++ src/nlp/ml_utils/src/ml_utils/data_utils/data_loader.py
@@ -0,0 +1,28 @@
+class DataLoader:
+    def __init__(self):
+        """
+        """
+
+    def load_data(self):
+        """
+
+        Returns
+        -------
+
+        """
+
+    def train_generator(self):
+        """
+        """
+
+    def val_generator(self):
+        """
+        """
+
+    def test_generator(self):
+        """
+        """
+
+    def save_object(self):
+        """
+        """
