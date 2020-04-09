4fde5f93595417ca5e7cd444e7ecd44020a4b31b
Florian Ludwig <florian.ludwig@konicaminolta.eu>
2020-04-09 21:42:05 +0200

    Included the bce_loss_pytorch function into the __init__.py file

--- src/nlp/ml_utils/__init__.py
+++ src/nlp/ml_utils/__init__.py
@@ -1,3 +1,4 @@
 from .src.ml_utils.experiment_structure.structure_utils import init_experiment_dir
 from .src.ml_utils.data_utils import data_split, statistics, data_loader
+from .src.ml_utils.loss_functions.bce_loss import bce_loss_pytorch
 from .src.ml_utils.logging import tb_log
