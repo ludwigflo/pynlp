1bfa5835b7d87e83ce5314038244f77bd9e88185
Florian Ludwig <florian.ludwig@konicaminolta.eu>
2020-04-10 13:05:29 +0200

    Updated __init__.py

--- src/nlp/ml_utils/__init__.py
+++ src/nlp/ml_utils/__init__.py
@@ -1,4 +1,4 @@
-from .src.ml_utils.experiment_structure.structure_utils import init_experiment_dir
+from .src.ml_utils.experiment_structure.structure_utils import init_experiment_dir, copy_files
 from .src.ml_utils.data_utils import data_split, statistics, data_loader
 from .src.ml_utils.loss_functions.bce_loss import bce_loss_pytorch
 from .src.ml_utils.metrics.accurracy import accuracy_pytorch
